107fa04047e7dc054c5c8a61b2e7833f594fb6bf
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-06-27 09:38:05 +0530

    Update TextFileIO.scala

--- spark/spark-shell/TextFileIO.scala
+++ spark/spark-shell/TextFileIO.scala
@@ -1,9 +1,11 @@
 import org.apache.spark.{SparkContext,SparkConf}
 
+object TextFileIO extends App{
   val conf = new SparkConf().setAppName("spark-submit textfileIO")
   val sc   = new SparkContext(conf)
   
   val textRDD = sc.textFile("/user/cloudera/sqoop_import/departments_txt")
-  //println(textRDD.count())
-  //textRDD.take(5) foreach println
+  println(textRDD.count())
+  textRDD.take(5) foreach println
   textRDD.saveAsTextFile("/user/cloudera/spark-shell/spark-submit/departments_txt")
+}
