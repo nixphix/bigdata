f9e9efb216454992845410c3df0f595659869dc2
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-06-27 09:43:04 +0530

    Update SetUpSBT.sh

--- spark/spark-shell/SetUpSBT.sh
+++ spark/spark-shell/SetUpSBT.sh
@@ -1,2 +1,15 @@
 curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 sudo yum install sbt
+ 
+cd /home/cloudera/spark/spark-shell/spark-submit/
+vi textfileio.sbt
+
+mkdir -p src/main/scala
+cd src/main/scala
+
+vi TextFileIO.scala
+
+cd /home/cloudera/spark/spark-shell/spark-submit/
+# sbt will download dependencies
+sbt
+package
