c66ff6b71e4b672b4e91ff0d504b0c664ea067e0
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-06-29 02:23:57 +0530

    Update SetUpSBT.sh

--- spark/spark-shell/SetUpSBT.sh
+++ spark/spark-shell/SetUpSBT.sh
@@ -13,3 +13,22 @@ cd /home/cloudera/spark/spark-shell/spark-submit/
 # sbt will download dependencies
 sbt
 package
+# jar file will be created in 
+#/home/cloudera/spark/spark-shell/spark-submit/target/scala-2.10/spark-submit-text-file-io_2.10-1.0.jar
+
+# remove the folder if it exist
+hdfs dfs -ls /user/cloudera/spark-shell/spark-submit/departments_txt
+hdfs dfs -rm -R /user/cloudera/spark-shell/spark-submit/departments_txt
+hdfs dfs -ls /user/cloudera/spark-shell/spark-submit/departments_txt
+
+# submit the job
+spark-submit \
+--master local \
+/home/cloudera/spark/spark-shell/spark-submit/target/scala-2.10/spark-submit-text-file-io_2.10-1.0.jar
+
+# check output
+hdfs dfs -ls /user/cloudera/spark-shell/spark-submit/departments_txt
+hdfs dfs -cat /user/cloudera/spark-shell/spark-submit/departments_txt/*
+
+
+
