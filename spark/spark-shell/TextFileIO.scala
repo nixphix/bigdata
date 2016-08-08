e0fb398c1d527debd34b123b15773d2b4016c410
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-08-08 08:31:37 +0530

    Update TextFileIO.scala

--- spark/spark-shell/TextFileIO.scala
+++ spark/spark-shell/TextFileIO.scala
@@ -1,11 +1,14 @@
 import org.apache.spark.{SparkContext,SparkConf}
+import java.time.ZoneDateTime
+import java.time.format.DateTimeFormatter
 
 object TextFileIO extends App{
   val conf = new SparkConf().setAppName("spark-submit textfileIO")
   val sc   = new SparkContext(conf)
+  def getTimeStamp: String = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").format(ZonedDateTime.now())
   
   val textRDD = sc.textFile("/user/cloudera/sqoop_import/departments_txt")
   println(textRDD.count())
   textRDD.take(5) foreach println
-  textRDD.saveAsTextFile("/user/cloudera/spark-shell/spark-submit/departments_txt")
+  textRDD.saveAsTextFile("/user/cloudera/spark-shell/spark-submit/departments_txt_"+getTimeStamp)
 }
