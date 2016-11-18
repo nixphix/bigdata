e065a3f6b59385281daa7874b2280036063b05a6
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-18 08:46:57 +0530

    Update ddl.sql

--- hive/hql/ddl.sql
+++ hive/hql/ddl.sql
@@ -1,1 +1,18 @@
 --hive --version
+--list databases
+SHOW DATABASES;
+
+--create a database
+CREATE DATABASE IF NOT EXISTS RETAIL_DB
+COMMENT 'This is Retail DB';
+
+--list databases
+SHOW DATABASES;
+
+--switch database
+USE RETAIL_DB;
+SELECT CURRENT_DATABASE();
+
+--check folder created for retail db
+dfs -ls /user/hive/warehouse/;
+dfs -ls /user/hive/warehouse/retail_db.db;
