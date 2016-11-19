2d4fb563b0de72fb11c3fb11aa81b844c42401c9
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-19 09:35:36 +0530

    Update ddl.sql

--- hive/hql/ddl.sql
+++ hive/hql/ddl.sql
@@ -16,3 +16,14 @@ SELECT CURRENT_DATABASE();
 --check folder created for retail db
 dfs -ls /user/hive/warehouse/;
 dfs -ls /user/hive/warehouse/retail_db.db;
+
+--create a table
+CREATE TABLE STORE (
+STORE_ID INT COMMENT 'This is the id assigned to store',
+STORE_LOCATION STRING COMMENT 'This is the location of store'
+)
+COMMENT 'Store Table'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '|'
+-- descibe 
+DESCRIBE RETAIL_DB
