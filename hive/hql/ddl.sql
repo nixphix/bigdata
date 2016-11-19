c55a8cf5271ef6010a673f6f5062d5336d08a797
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-19 17:12:10 +0530

    Update ddl.sql

--- hive/hql/ddl.sql
+++ hive/hql/ddl.sql
@@ -3,14 +3,14 @@
 SHOW DATABASES;
 
 --create a database
-CREATE DATABASE IF NOT EXISTS RETAIL_DB
+CREATE DATABASE IF NOT EXISTS retail_db
 COMMENT 'This is Retail DB';
 
 --list databases
 SHOW DATABASES;
 
 --switch database
-USE RETAIL_DB;
+USE retail_db;
 SELECT CURRENT_DATABASE();
 
 --check folder created for retail db
@@ -18,12 +18,52 @@ dfs -ls /user/hive/warehouse/;
 dfs -ls /user/hive/warehouse/retail_db.db;
 
 --create a table
-CREATE TABLE STORE (
-STORE_ID INT COMMENT 'This is the id assigned to store',
-STORE_LOCATION STRING COMMENT 'This is the location of store'
+CREATE TABLE store (
+store_id INT COMMENT 'This is the id assigned to store',
+store_location STRING COMMENT 'This is the location of store'
 )
 COMMENT 'Store Table'
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '|'
--- descibe 
-DESCRIBE RETAIL_DB
+STORED AS TEXTFILE;
+
+-- describe
+DESCRIBE store;
+DESCRIBE FORMATTED store;
+
+/*# CREATE INPUT DATA
+**# CREATE A CSV FILE
+**cat > store.csv
+**1,chennai
+**2,mumbai
+**3,kolkata
+**4,delhi
+**<Ctrl+C>
+**# CREATE A PSV FILE
+**sed 's/,/|/g' store.csv | cat > store.psv
+*/
+
+-- load date into hive table
+-- load csv instead of psv
+LOAD DATA LOCAL INPATH '/home/cloudera/hive/data/store.csv' INTO TABLE retail_db.store;
+SELECT * FROM retail_db.store;
+dfs -ls /user/hive/warehouse/retail_db.db/store/;
+dfs -cat /user/hive/warehouse/retail_db.db/store/store.csv;
+
+-- now load psv
+TRUNCATE TABLE retail_db.store;
+SELECT * FROM retail_db.store;
+LOAD DATA LOCAL INPATH '/home/cloudera/hive/data/store.psv' INTO TABLE retail_db.store;
+SELECT * FROM retail_db.store;
+dfs -ls /user/hive/warehouse/retail_db.db/store/;
+dfs -cat /user/hive/warehouse/retail_db.db/store/store.psv;
+
+USE retail_db;
+DESCRIBE FORMATTED store;
+
+
+
+
+
+
+
