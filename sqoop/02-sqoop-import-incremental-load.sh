21a35e08377cef120bcf59a1881f60081927ffc2
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-21 09:57:37 +0530

    Update 02-sqoop-import-incremental-load.sh

--- sqoop/02-sqoop-import-incremental-load.sh
+++ sqoop/02-sqoop-import-incremental-load.sh
@@ -16,8 +16,9 @@ sqoop import \
 --connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
 --username retail_dba \
 --password cloudera \
---table orders \
 --as-textfile \
+--table orders \
+--columns  order_id,order_date,order_customer_id,order_status \
 --where 'EXTRACT(YEAR FROM order_date) = 2013 AND EXTRACT(MONTH FROM order_date) = 08' \
 --target-dir /user/cloudera/staging/sq_import/retail_db/orders_partition/ \
 --append \
@@ -25,7 +26,33 @@ sqoop import \
 
 # check data in hdfs
 hdfs dfs -ls -R /user/cloudera/staging/sq_import/retail_db/orders_partition/
+hdfs dfs -cat /user/cloudera/staging/sq_import/retail_db/orders_partition/part*
 
-hdfs dfs -cat /user/cloudera/staging/sq_import/retail_db/orders/year*month=07/part*
-hdfs dfs -cat /user/cloudera/staging/sq_import/retail_db/orders/year*month=08/part*
+# third load
+sqoop import \
+--connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
+--username retail_dba \
+--password cloudera \
+--as-textfile \
+--table orders \
+--target-dir /user/cloudera/staging/sq_import/retail_db/orders_partition \
+--append \
+--num-mappers 1
+
+hdfs dfs -ls -R /user/cloudera/staging/sq_import/retail_db/orders_partition/
+hdfs dfs -cat   /user/cloudera/staging/sq_import/retail_db/orders_partition/part*2
+
+# cleanup
+#hdfs dfs -rm -R /user/cloudera/staging/sq_import/retail_db/orders_partition
+#hdfs dfs -rm /user/cloudera/staging/sq_import/retail_db/orders_partition/part*2
+
+
+
+
+
+
+
+
+
+
 
