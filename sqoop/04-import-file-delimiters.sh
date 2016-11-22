93e0ee764ee6387964c28c1018064de89888f7f4
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-23 03:46:17 +0530

    Update 04-import-file-delimiters.sh

--- sqoop/04-import-file-delimiters.sh
+++ sqoop/04-import-file-delimiters.sh
@@ -1,4 +1,4 @@
-#-----------  using field and line delimiters  -----------#
+#-----------  using field, line delimiters and null replacement  -----------#
 # data preparation in mysql
 # mysql -u retail_dba -pcloudera -h quickstart.cloudera retail_db
 # create table delimiters_test(id int not null auto_increment, text varchar(100), null_num int default null, primary key (id));
@@ -23,3 +23,75 @@ sqoop import \
 # check in hdfs 
 hdfs dfs -ls -R /user/cloudera/staging/sq_import/retail_db/delimiters_test
 hdfs dfs -cat   /user/cloudera/staging/sq_import/retail_db/delimiters_test/part-m-00000
+
+
+# import the table with \001 as field & \n as line delimiter and use \N for null values which is the default in hive
+sqoop import \
+--connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
+--username retail_dba \
+--password cloudera \
+--table delimiters_test \
+--fields-terminated-by '\001' \
+--lines-terminated-by '\n' \
+--null-non-string '\\N' \
+--null-string '\\N' \
+--warehouse-dir /user/cloudera/staging/sq_import/retail_db \
+--delete-target-dir \
+-m 1
+
+# check in hdfs 
+hdfs dfs -ls -R /user/cloudera/staging/sq_import/retail_db/delimiters_test
+hdfs dfs -cat   /user/cloudera/staging/sq_import/retail_db/delimiters_test/part-m-00000
+
+#-----------  using escape and enclosed options  -----------#
+# data preparation in mysql
+# insert into delimiters_test value(4,'hello, world!',99);
+
+# using escape literal to escape comma in text field
+sqoop import \
+--connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
+--username retail_dba \
+--password cloudera \
+--table delimiters_test \
+--as-textfile \
+--escaped-by \\ \
+--optionally-enclosed-by \" \
+--warehouse-dir /user/cloudera/staging/sq_import/retail_db \
+--delete-target-dir \
+--num-mappers 1
+
+# check in hdfs 
+hdfs dfs -cat   /user/cloudera/staging/sq_import/retail_db/delimiters_test/part-m-00000
+
+
+# using enclose literal to enclose all the fields 
+sqoop import \
+--connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
+--username retail_dba \
+--password cloudera \
+--table delimiters_test \
+--as-textfile \
+--enclosed-by \" \
+--warehouse-dir /user/cloudera/staging/sq_import/retail_db \
+--delete-target-dir \
+--num-mappers 1
+
+# check in hdfs 
+hdfs dfs -cat   /user/cloudera/staging/sq_import/retail_db/delimiters_test/part-m-00000
+
+
+# using enclosed optionally literal to enclose the text having comma
+sqoop import \
+--connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
+--username retail_dba \
+--password cloudera \
+--table delimiters_test \
+--as-textfile \
+--optionally-enclosed-by \" \
+--warehouse-dir /user/cloudera/staging/sq_import/retail_db \
+--delete-target-dir \
+--num-mappers 1
+
+# check in hdfs 
+hdfs dfs -cat   /user/cloudera/staging/sq_import/retail_db/delimiters_test/part-m-00000
+
