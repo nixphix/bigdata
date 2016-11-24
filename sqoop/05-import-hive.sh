f2c0a58d266fc9378f1748edc67a05a3dc81b509
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-24 08:34:04 +0530

    Update 05-import-hive.sh

--- sqoop/05-import-hive.sh
+++ sqoop/05-import-hive.sh
@@ -1,1 +1,15 @@
 #--------------------  hive import  --------------------#
+#--- data preparation in mysql
+# create table hive_import as select * from delimiters_test;
+# select * from hive_import;
+
+#--- first import
+sqoop import \
+--connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
+--username retail_dba \
+--password cloudera \
+--table hive_import \
+--hive-import \
+--create-hive-table \
+--hive-table retail_db.hive_import \
+--num-mappers 1
