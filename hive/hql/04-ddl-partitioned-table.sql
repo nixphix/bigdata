f314e2c551c1bf551e76481466ee59721b7606b3
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-26 20:24:36 +0530

    Update 04-ddl-partitioned-table.sql

--- hive/hql/04-ddl-partitioned-table.sql
+++ hive/hql/04-ddl-partitioned-table.sql
@@ -30,9 +30,9 @@ SELECT * FROM retail_db.orders_part where order_month='2013-09' limit 10;
 dfs -ls -R hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_part;
 
 
--------------------------------------------------
---  creating partitioned table in strict mode  --
--------------------------------------------------
+----------------------------------------------------------------------
+--  creating partitioned table with two columns in non strict mode  --
+----------------------------------------------------------------------
 
 -- create table with two partition column
 CREATE TABLE IF NOT EXISTS retail_db.orders_part_2 (
@@ -50,5 +50,36 @@ INSERT INTO retail_db.orders_part_2 PARTITION (order_month,order_year)
 SELECT order_id, to_date(order_date) order_date, order_customer_id,
 order_status, month(order_date) order_month, year(order_date) order_year FROM default.orders;
 
+-- check the data
+SELECT * FROM retail_db.orders_part_2 limit 10;
+SELECT * FROM retail_db.orders_part_2 where order_month=09 limit 10;
+
+-- check data in hdfs, notice that the folder is arranged as order_month/order_year
+dfs -ls -R hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_part_2;
 
 
+-- create table with two partition column
+CREATE TABLE IF NOT EXISTS retail_db.orders_part_3 (
+order_id int,
+order_date date,
+order_customer_id int,
+order_status string )
+PARTITIONED BY (order_year int, order_month int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
+-- load data from existing table in staging.orders
+INSERT INTO retail_db.orders_part_3 PARTITION (order_month,order_year)
+SELECT order_id, to_date(order_date) order_date, order_customer_id,
+order_status, month(order_date) order_month, year(order_date) order_year FROM default.orders;
+
+-- check data in hdfs 
+-- notice that the folder is arranged as order_year/order_month but still there are 12 year folder !!! 
+-- the orders in which you load also matters, in the insert statement month is feed first and then year second
+-- so be mindfull of the order in which the partition columns are declared and loaded
+dfs -ls hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_part_3;
+
+-- check the data
+-- this query will not return any value
+SELECT * FROM retail_db.orders_part_3 where order_month=09 limit 10;
