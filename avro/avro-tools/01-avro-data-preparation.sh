042182f32728302b4722ecc7febb131c024a620f
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-11-29 22:27:55 +0530

    Update 01-avro-data-preparation.sh

--- avro/avro-tools/01-avro-data-preparation.sh
+++ avro/avro-tools/01-avro-data-preparation.sh
@@ -18,40 +18,39 @@ sqoop import \
 
 # check data in hdfs and copy it to local
 hdfs dfs -ls -R /user/cloudera/staging/avro/retail_db/departments
-hdfs dfs -get /user/cloudera/staging/avro/retail_db/departments/part-m-00000.avro ~/data/avro
+hdfs dfs -get /user/cloudera/staging/avro/retail_db/departments/part-m-00000.avro ~/data/avro/part-m-00000-departments.avro
 
 # check local file for java, avsc and avro file
 ls -ltr
-cat departments.avsc
-view departments.avro
+cat sqoop_import_departments.avsc
+view part-m-00000-departments.avro
 
 
 #--- second import
 #--- preparing data in mysql
 mysql -u retail_dba -pcloudera retail_db
 create table dept_avro as select * from departments ;
-select * from dept;
-desc dept;
-alter table dept alter department_id set default -1
-desc dept;
+select * from dept_avro;
+desc dept_avro;
+alter table dept_avro alter department_id set default -1;
+desc dept_avro;
 
 # import departments table as avro file
 sqoop import \
 --connect jdbc:mysql://quickstart.cloudera:3306/retail_db \
 --username retail_dba \
 --password cloudera \
---table dept \
+--table dept_avro \
 --as-avrodatafile \
---target-dir /user/cloudera/staging/avro/retail_db/dept \
+--target-dir /user/cloudera/staging/avro/retail_db/dept_avro \
 -m 1
 
 # check data in hdfs and copy it to local
-hdfs dfs -ls -R /user/cloudera/staging/avro/retail_db/dept
-hdfs dfs -get /user/cloudera/staging/avro/retail_db/dept/part-m-00000.avro ~/data/avro
+hdfs dfs -ls -R /user/cloudera/staging/avro/retail_db/dept_avro
+hdfs dfs -get /user/cloudera/staging/avro/retail_db/dept_avro/part-m-00000.avro ~/data/avro/part-m-00000-dept.avro
 
 # check local file for avsc and avro file
 # note that eventhough the source table has -1 as default value for department_id avro file indicates null as default
 ls -ltr
-cat dept.avsc
-view dept.avro
-
+cat sqoop_import_dept_avro.avsc
+view part-m-00000-dept.avro
