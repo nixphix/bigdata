26f00144846c41cf9ca5ee1cfa0ee7bfe1ce631c
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-12-01 02:50:04 +0530

    Update 08-ddl-avro-schema-partition.sql

--- hive/hql/08-ddl-avro-schema-partition.sql
+++ hive/hql/08-ddl-avro-schema-partition.sql
@@ -18,4 +18,38 @@ TBLPROPERTIES ('avro.schema.url'='/user/cloudera/staging/avro/schema/orders/sqoo
 --- desc the table
 DESCRIBE FORMATTED retail_db.orders_avro_part;
 
---- load data into table
+--- check hive partition settings, mode will be strict
+set hive.exec.dynamic.partition;
+set hive.exec.dynamic.partition.mode;
+
+--- add partition, try 9 & 09 and see which one gets the data
+ALTER TABLE retail_db.orders_avro_part ADD PARTITION(order_month=09);
+ALTER TABLE retail_db.orders_avro_part ADD PARTITION(order_month=9);
+
+--- check partition folders
+dfs -ls hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_avro_part;
+
+--- load data into table from retail_db.orders_avro
+FROM retail_db.orders_avro
+INSERT INTO retail_db.orders_avro_part PARTITION (order_month=9)
+SELECT * 
+WHERE month(FROM_UNIXTIME(cast(order_date/1000 as int))) = 9;
+
+--- check partition folders
+dfs -ls -R hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_avro_part;
+dfs -rm -R hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_avro_part/order_month=09;
+
+--- now switch partition mode to nonstrict to load dynamically
+set hive.exec.dynamic.partition.mode=nonstrict;
+
+--- lets load data for rest of the month
+FROM retail_db.orders_avro o
+INSERT INTO retail_db.orders_avro_part PARTITION (order_month)
+SELECT o.*, month(FROM_UNIXTIME(cast(o.order_date/1000 as int))) order_month
+WHERE month(FROM_UNIXTIME(cast(o.order_date/1000 as int))) <> 9;
+
+--- check folders and files on hdfs
+dfs -ls -R hdfs://quickstart.cloudera:8020/user/hive/warehouse/retail_db.db/orders_avro_part;
+
+--- check data in hive
+SELECT * FROM retail_db.orders_avro_part limit 10;
