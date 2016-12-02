fcfe52402073d206b606d0f057c5e093c4a55182
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-12-02 17:47:14 +0530

    Create 09-ddl-evolve-hive-table.sql

---
+++ hive/hql/09-ddl-evolve-hive-table.sql
@@ -0,0 +1,5 @@
+-----------------------------------------------------------------------
+--  creating hive table with avro format and then evolve the schema  --
+-----------------------------------------------------------------------
+
+--- create avro schema for a new table
