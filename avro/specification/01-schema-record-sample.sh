07002e8038a2a6f016b7e94e69f39265dbaafa18
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-12-02 21:59:51 +0530

    Update 01-schema-record-sample.sh

--- avro/specification/01-schema-record-sample.sh
+++ avro/specification/01-schema-record-sample.sh
@@ -22,4 +22,13 @@ cat > employee_new_schema.avsc
                {"name" : "employee_dob", "type" : "long", "default" : 637804800} ]
 }
 
+#--- generate some random data for these schema
+avro-tools random --schema-file employee.avsc --count 5 employee.avro
+avro-tools random --schema-file employee_new_schema.avsc --count 5 employee_new_schema.avro
+
 #--- copy the files to hdfs
+ls -ltr
+hdfs dfs -mkdir -p /user/cloudera/staging/avro/schema/employee
+hdfs dfs -mkdir -p /user/cloudera/staging/avro/data/employee
+hdfs dfs -put employee*.avsc /user/cloudera/staging/avro/schema/employee
+hdfs dfs -put employee*.avro /user/cloudera/staging/avro/data/employee
